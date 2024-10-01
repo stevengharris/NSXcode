806d5209b6ea06466be3cce375a6d9c002f60d05
Steven G. Harris <steven.g.harris@gmail.com>
2024-10-01 09:25:41 -0700

    Initial Commit

---
+++ MyProduct/MyProductApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyProductApp.swift
+//  MyProduct
+//
+//  Created by Steven Harris on 10/1/24.
+//
+
+import SwiftUI
+
+@main
+struct MyProductApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
